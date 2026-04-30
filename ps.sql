b775f31c1fca1d4f4d274a11e66328180c6db1db
Preeti Singh <preeti1017singh@gmail.com>
2026-04-30 21:08:52 +0530

    Update ps.sql

--- ps.sql
+++ ps.sql
@@ -1,13 +1,13 @@
--- create new_schema ;
+
 USE new_schema;
 
--- Create the authors table
+
 CREATE TABLE authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(50)
 );
 
--- Create the books table
+
 CREATE TABLE books (
     book_id INT PRIMARY KEY,
     book_title VARCHAR(100),
@@ -15,14 +15,14 @@ CREATE TABLE books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- Insert data into the authors table
+
 INSERT INTO authors (author_id, author_name)
 VALUES 
 (1, 'J.K. Rowling'),
 (2, 'George R.R. Martin'),
 (3, 'J.R.R. Tolkien');
 
--- Insert data into the books table
+
 INSERT INTO books (book_id, book_title, author_id)
 VALUES 
 (101, 'Harry Potter and the Sorcerer\'s Stone', 1),
@@ -32,7 +32,7 @@ VALUES
 (105, 'The Fellowship of the Ring', 3),
 (106, 'The Two Towers', 3);
 
--- Join the tables and retrieve data
+
 SELECT 
     books.book_id, 
     books.book_title, 
